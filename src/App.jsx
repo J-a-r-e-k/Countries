a6e708a73de46af68c2bb3bdc48ac990c3798532
Jarosław K <jarasss1@wp.pl>
2024-06-26 22:21:19 +0200

    ReactRouter configuration nesting added

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,25 @@ import { Routes, Route, Outlet, Link } from 'react-router-dom';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
-// const Layout = () => {
-//   return (
-//     <div>
-//       {/* A "layout route" is a good place to put markup you want to
-//       share across all the pages on your site, like navigation. */}
+const Layout = () => {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+      share across all the pages on your site, like navigation. */}
+      <Header />
 
-//       {/* An <Outlet> renders whatever child route is currently active,
-//       so you can think about this <Outlet> as a placeholder for
-//       the child routes we defined above. */}
-//       <Outlet />
-//     </div>
-//   );
-// };
+      {/* An <Outlet> renders whatever child route is currently active,
+      so you can think about this <Outlet> as a placeholder for
+      the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Outlet />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path=":country" element={<CountryPage />} />
 
