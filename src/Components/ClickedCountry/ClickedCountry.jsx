9fb540a1d3c3b621c0f8d391f56d8ab01a6c0ccf
Jarosław K <jarasss1@wp.pl>
2024-07-08 12:53:40 +0200

    improved function returning display country information by name

--- src/Components/ClickedCountry/ClickedCountry.jsx
+++ src/Components/ClickedCountry/ClickedCountry.jsx
@@ -19,7 +19,7 @@ const ClickedCountry = ({ countryName }) => {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      setCountrie(data);
+      setCountrie(data[0]);
 
       getBorderCountries(data[0].borders); // funkcja z przekazaniem tablicy kodów //
     } catch (error) {
@@ -53,6 +53,7 @@ const ClickedCountry = ({ countryName }) => {
     getCountriesData();
   }, []);
 
+  // czy da sie ustawić zmiane kraju tak by cofając przechodziło przez wcześniejsze?//
   const borderBtn = () =>
     borderCountries.map((element) => {
       return (
@@ -70,77 +71,74 @@ const ClickedCountry = ({ countryName }) => {
       );
     });
 
-  // const returnsCountry = country.map((country, index) => {
-
-  const returnsCountry = country.map((country, index) => {
-    const currencie = Object.values(country.currencies)[0].name;
-    const language = Object.values(country.languages).map((nameLanguage) => {
-      return <span>{nameLanguage} </span>;
-    });
-
-    return (
-      <div className={Style.contry} key={index}>
-        <button
-          className={Style.btnContry}
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 32 32"
-            preserveAspectRatio="xMidYMid meet"
+  const returnsCountry = () => {
+    if (country.length !== 0) {
+      const currencie = Object.values(country.currencies)[0].name;
+      const language = Object.values(country.languages).map((nameLanguage) => {
+        return <span>{nameLanguage} </span>;
+      });
+      return (
+        <div className={Style.contry}>
+          <button
+            className={Style.btnContry}
+            onClick={() => {
+              navigate(-1);
+            }}
           >
-            <path d="M32 15H3.41l8.29-8.29-1.41-1.42-10 10a1 1 0 0 0 0 1.41l10 10 1.41-1.41L3.41 17H32z" />
-          </svg>
-          Back
-        </button>
-        <div className={Style.wrapFlag}>
-          <img
-            className={Style.flag}
-            src={country.flags.svg}
-            alt={country.flags.alt}
-          />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 32 32"
+              preserveAspectRatio="xMidYMid meet"
+            >
+              <path d="M32 15H3.41l8.29-8.29-1.41-1.42-10 10a1 1 0 0 0 0 1.41l10 10 1.41-1.41L3.41 17H32z" />
+            </svg>
+            Back
+          </button>
+          <div className={Style.wrapFlag}>
+            <img
+              className={Style.flag}
+              src={country.flags.svg}
+              alt={country.flags.alt}
+            />
+          </div>
+          <div className={Style.wrapDescription}>
+            <h2 className={Style.countryName}>{country.name.common}</h2>
+            <div className={Style.wrapContent}>
+              <p className={Style.description}>
+                Native Name: <span>{country.name.official}</span>
+              </p>
+              <p className={Style.description}>
+                Population: <span>{country.population}</span>
+              </p>
+              <p className={Style.description}>
+                Region: <span>{country.region}</span>
+              </p>
+              <p className={Style.description}>
+                Sub Region: <span>{country.subregion}</span>
+              </p>
+              <p className={Style.description}>
+                Capital: <span>{country.capital}</span>
+              </p>
+            </div>
+            <div className={Style.wrapContent}>
+              <p className={Style.description}>
+                Top Level Domain: <span>{country.cca2}</span>
+              </p>
+              <p className={Style.description}>
+                Currencies: <span>{currencie}</span>
+              </p>
+              <p className={Style.description}>Languages: {language}</p>
+            </div>
+            <div className={Style.wrapBorderBtn}>
+              <p className={Style.description}>Border Countries:</p>
+              <div className={Style.wrapCountries}> {borderBtn()}</div>
+            </div>
+          </div>
         </div>
-
-        <div className={Style.wrapDescription}>
-          <h2 className={Style.countryName}>{country.name.common}</h2>
-          <div className={Style.wrapContent}>
-            <p className={Style.description}>
-              Native Name: <span>{country.name.official}</span>
-            </p>
-            <p className={Style.description}>
-              Population: <span>{country.population}</span>
-            </p>
-            <p className={Style.description}>
-              Region: <span>{country.region}</span>
-            </p>
-            <p className={Style.description}>
-              Sub Region: <span>{country.subregion}</span>
-            </p>
-            <p className={Style.description}>
-              Capital: <span>{country.capital}</span>
-            </p>
-          </div>
-          <div className={Style.wrapContent}>
-            <p className={Style.description}>
-              Top Level Domain: <span>{country.cca2}</span>
-            </p>
-            <p className={Style.description}>
-              Currencies: <span>{currencie}</span>
-            </p>
-            <p className={Style.description}>Languages: {language}</p>
-          </div>
-          <div className={Style.wrapBorderBtn}>
-            <p className={Style.description}>Border Countries:</p>
-            <div className={Style.wrapCountries}> {borderBtn()}</div>
-          </div>
-        </div>
-      </div>
-    );
-  });
-
-  return <>{returnsCountry}</>;
+      );
+    }
+  };
+  return returnsCountry();
 };
 
 export default ClickedCountry;
